553b21b44ca5948c5e48496f07043e7f0930a540
guilhermesouza01 <ghostgui60@gmail.com>
2023-03-13 16:49:50 -0300

    create the main function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="title">App</div>;
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+  const [value, setValue] = useState("");
+
+  const eraseInput = () => {
+    setValue("");
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(value);
+    eraseInput();
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.which === ENTER_KEY) {
+      handleSubmit();
+    } else if (event.which === ESCAPE_KEY) {
+      eraseInput();
+    }
+  };
+  return (
+    <section id="app" className="container">
+      <header>
+        <div className="title">Todo</div>
+      </header>
+      <section className="main">
+        <input
+          type="text"
+          className="new-todo"
+          placeholder="o que precisar ser feito?"
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyPress}
+        />
+      </section>
+    </section>
+  );
 }
 
 export default App;
